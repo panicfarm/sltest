5c7329f8c5779a4d580f82c812d2d3ff5984b4b8
Alec Matusis <matusis@matusis.com>
2019-11-14 01:07:35 -0800

    Added tempfile for convenience

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 use rand::Rng;
 use sled::*;
+use tempfile::tempdir;
 
 fn main() {
+    let dir = tempdir().unwrap();
+
     let config = sled::Config::default()
         .cache_capacity(256)
-        .path("/tmp/sltest.0");
-    println!("{:?}", config);
+        .snapshot_after_ops(1_000_000_000) //never
+        .path(dir.path());
+    println!("{:#?}", config);
     let db = config.open().unwrap();
-    if !db.is_empty() {
-        panic!("Empty DB!!!")
-    }
-    let mut key;
-    let mut val;
-    let mut n = 0;
-    loop {
-        key = rand::thread_rng().gen::<[u8; 32]>();
-        val = rand::thread_rng().gen::<[u8; 32]>();
+    for n in 0.. {
+        let key = rand::thread_rng().gen::<[u8; 32]>();
+        let val = rand::thread_rng().gen::<[u8; 32]>();
         db.insert(key.to_vec(), val.to_vec());
-        n += 1;
         println!("{}", n);
     }
 }
